4c6b9e2186f9baeee0c974f019ce7b3a68ab645e
davisuga <daviciencia1@gmail.com>
2023-07-05 17:30:12 -0300

    Auth fix

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,13 @@
 import Chat from "@/components/Chat";
 import { ClientProviders } from "../client-providers";
+import { auth, currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const user = auth();
+  if (!user.userId) {
+    redirect("/sign-in");
+  }
   return (
     <ClientProviders>
       <Chat />
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/sign-in"],
+  publicRoutes: ["/", "/sign-in", "/chat"],
 });
 
 export const config = {
