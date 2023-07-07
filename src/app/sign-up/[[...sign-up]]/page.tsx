5bbdb085bdfabb565ffdcea64d872157d88054a9
davisuga <daviciencia1@gmail.com>
2023-07-07 17:08:51 -0300

    Fixing ugly sign in again

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignIn } from "@clerk/nextjs/app-beta";
+import { SignUp } from "@clerk/nextjs/app-beta";
 
 export default function Page() {
   return (
     <div className="flex flex-1 items-center justify-center">
-      <SignIn redirectUrl={"/chat"} />
+      <SignUp />
     </div>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  signInUrl: "/sign-in",
+  publicRoutes: ["/", "/sign-in", "/sign-up"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
