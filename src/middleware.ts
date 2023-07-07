4c04f76d917a9d5bf2afb3ab759af87941d0cf27
davisuga <daviciencia1@gmail.com>
2023-07-07 16:54:18 -0300

    bring matcher back

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,6 @@
 import { authMiddleware, withClerkMiddleware } from "@clerk/nextjs";
 
 export default withClerkMiddleware();
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
