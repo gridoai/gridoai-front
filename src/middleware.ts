3a9feff9d55689d14f64491fe7d2e00842b1ab56
davisuga <daviciencia1@gmail.com>
2023-07-07 16:49:46 -0300

    removed matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,3 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ["/", "/sign-in", "/chat"],
 });
-
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
