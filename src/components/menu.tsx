423bd907b740d40ad7fe40cba94406ccc9a5e120
davisuga <daviciencia1@gmail.com>
2023-07-08 11:02:19 -0300

    Create and list working

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,14 +1,5 @@
 import { Button } from "@/components/ui/button";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { OrganizationSwitcher } from "@clerk/nextjs";
 import { MenuIcon } from "lucide-react";
 import Link from "next/link";
@@ -17,16 +8,12 @@ export function SideMenu() {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button
-          variant="outline"
-          size="icon"
-          //   className="h-16 w-16 rounded-full"
-        >
+        <Button variant="outline" size="icon">
           <MenuIcon />
         </Button>
       </SheetTrigger>
       <SheetContent className="border-none bg-neutral-0 flex flex-col justify-end gap-4 items-end">
-        <OrganizationSwitcher />
+        <OrganizationSwitcher defaultOpen />
         <Link href="/documents">
           <Button variant="outline">Manage documents</Button>
         </Link>
