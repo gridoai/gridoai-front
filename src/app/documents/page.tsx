1ccb63c1d4910c2a88c65311e461a47f573b5338
davisuga <daviciencia1@gmail.com>
2023-07-11 20:15:19 -0300

    removed window mutation

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -92,7 +92,6 @@ const DocumentsList: React.FC = () => {
   const { isLoading, fetchStatus, refetch } = refineCore.tableQueryResult;
   const loading = isLoading || fetchStatus === "fetching";
 
-  window.refineCore = refineCore;
   console.log({ pageSize, current, pageCount });
   return (
     <div className={` flex flex-col bg-neutral-1 p-4 rounded-xl`}>
