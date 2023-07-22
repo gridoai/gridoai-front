03dbf3cf1be9d51cc1c01c29dabeb629e23f544b
davisuga <daviciencia1@gmail.com>
2023-07-22 17:58:20 -0300

    Using proxy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const { withAxiom } = require("next-axiom");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
+  async rewrites() {
+    return [
+      {
+        source: "/remote/:path*",
+        destination: process.env.REMOTE_SERVER + "/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = withAxiom(nextConfig);
