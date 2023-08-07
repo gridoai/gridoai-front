c914517834152d0e45e5af051221af60c4f91aa3
davisuga <daviciencia1@gmail.com>
2023-08-07 19:07:37 -0300

    next update

--- src/components/toast.tsx
+++ src/components/toast.tsx
@@ -1,6 +1,6 @@
 import * as ToastPrimitives from "@radix-ui/react-toast";
 import { cva, type VariantProps } from "class-variance-authority";
-import { X } from "lucide-react";
+import { X } from "@phosphor-icons/react"
 import * as React from "react";
 import { twMerge } from "tailwind-merge";
 
@@ -42,7 +42,7 @@ const toastVariants = cva(
 const Toast = React.forwardRef<
   React.ElementRef<typeof ToastPrimitives.Root>,
   React.ComponentPropsWithoutRef<typeof ToastPrimitives.Root> &
-    VariantProps<typeof toastVariants>
+  VariantProps<typeof toastVariants>
 >(({ className, variant, ...props }, ref) => {
   return (
     <ToastPrimitives.Root
