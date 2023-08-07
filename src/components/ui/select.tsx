5e859c3a8f1908aa34c073c1ed61876635dce74c
davisuga <daviciencia1@gmail.com>
2023-08-07 19:07:37 -0300

    next update

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import * as SelectPrimitive from "@radix-ui/react-select";
-import { Check, ChevronDown } from "lucide-react";
+import { Check, CaretDown as ChevronDown } from "@phosphor-icons/react";
 
 import { cn } from "@/lib/utils";
 
@@ -26,7 +26,7 @@ const SelectTrigger = React.forwardRef<
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <ChevronDown className="h-4 w-4 opacity-50" />
+      <ChevronDown className="h-4 w-4 ml-1 opacity-50" />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ));
@@ -42,7 +42,7 @@ const SelectContent = React.forwardRef<
       className={cn(
         `backdrop-blur-lg bg-opacity-20 drop-shadow-lg elative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2`,
         position === `popper` &&
-          `data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1`,
+        `data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1`,
         className
       )}
       position={position}
@@ -52,7 +52,7 @@ const SelectContent = React.forwardRef<
         className={cn(
           `p-1`,
           position === `popper` &&
-            `h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]`
+          `h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]`
         )}
       >
         {children}
