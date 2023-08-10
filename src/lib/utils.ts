c33b41e8c5baec731d1a9232bb5e5383ba99c4f2
davisuga <daviciencia1@gmail.com>
2023-08-09 21:15:21 -0300

    Better upload strategy

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function asyncMap<T, U>(
+  arr: T[],
+  func: (item: T) => Promise<U>
+): Promise<U[]> {
+  const results: U[] = [];
+
+  for (const item of arr) {
+    await func(item).then(results.push).catch(console.error);
+  }
+
+  return results;
 }
