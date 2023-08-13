18d4b954a0430e4fe88fd96e34472caf8192880f
davisuga <daviciencia1@gmail.com>
2023-08-13 15:10:42 -0300

    Resize component

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -4,10 +4,17 @@ import { Message } from "@/types/Message";
 import rehypeRaw from "rehype-raw";
 
 import { useUser } from "@clerk/nextjs";
-import { FileText, PaperPlaneRight, Polygon, Spinner, User } from "@phosphor-icons/react";
+import {
+  FileText,
+  PaperPlaneRight,
+  Polygon,
+  Spinner,
+  User,
+} from "@phosphor-icons/react";
 import { useEffect, useRef, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import styles from "./index.module.css";
+import { AutoHeightTextarea } from "../ui/resizingTextArea";
 
 export default function Chat() {
   const [userInput, setUserInput] = useState(``);
@@ -80,12 +87,12 @@ export default function Chat() {
       newMsg,
     ]).catch(
       (e) =>
-      ({
-        error: e.message,
-      } as {
-        error: string;
-        message: never;
-      })
+        ({
+          error: e.message,
+        } as {
+          error: string;
+          message: never;
+        })
     );
 
     if (data.error) {
@@ -136,13 +143,21 @@ export default function Chat() {
       <div className={`${styles.cloud} flex flex-1`}>
         <div ref={messageListRef} className={styles.messagelist}>
           {messages.map((message, index) => {
-            const [content, sources] = message.message.split(`\n\n\n\nsources:`);
+            const [content, sources] =
+              message.message.split(`\n\n\n\nsources:`);
             console.log({
               content,
               sources,
-            })
+            });
             return (
-              <Message key={index} content={content} sources={sources} type={message.type} loading={loading} index={index} />
+              <Message
+                key={index}
+                content={content}
+                sources={sources}
+                type={message.type}
+                loading={loading}
+                index={index}
+              />
               // The latest message sent by the user will be animated while waiting for a response
               // <div
               //   key={index}
@@ -191,13 +206,11 @@ export default function Chat() {
         <div className={styles.cloudform}>
           <form onSubmit={handleSubmit}>
             <div className="p-4 flex gap-4 items-center flex-1 w-full border rounded-lg border-solid border-neutral-700 pr-2">
-              <textarea
+              <AutoHeightTextarea
                 disabled={loading}
                 onKeyDown={handleEnter}
                 ref={textAreaRef}
                 autoFocus={false}
-                rows={1}
-                maxLength={512}
                 id="userInput"
                 name="userInput"
                 placeholder={
@@ -205,7 +218,7 @@ export default function Chat() {
                 }
                 value={userInput}
                 onChange={(e) => setUserInput(e.target.value)}
-                className={`${styles.textarea}`}
+                className={`${styles.textarea} max-h-80`}
               />
 
               <button type="submit" disabled={loading}>
@@ -229,24 +242,22 @@ export const Message = ({
   sources,
   loading,
 }: {
-  content: string,
-  type: `userMessage` | `robot`,
-  sources: string,
-  loading: boolean,
-  index: number,
+  content: string;
+  type: `userMessage` | `robot`;
+  sources: string;
+  loading: boolean;
+  index: number;
 }) => {
-
   return (
     <div
-
-      className={`items-center ` +
-        (type === `userMessage` &&
-          loading
-          // &&                    index === messages.length - 1
-          ? styles.usermessagewaiting
+      className={
+        `items-center ` +
+        (type === `userMessage` && loading
+          ? // &&                    index === messages.length - 1
+            styles.usermessagewaiting
           : type === `robot`
-            ? styles.apimessage
-            : styles.usermessage)
+          ? styles.apimessage
+          : styles.usermessage)
       }
     >
       {/* Display the correct icon depending on the message type */}
@@ -259,21 +270,23 @@ export const Message = ({
       </div>
       <div className="flex flex-col gap-2 flex-1">
         <div className={styles.markdownanswer}>
-          <ReactMarkdown
-            rehypePlugins={[rehypeRaw]}
-            linkTarget={`_blank`}
-          >
+          <ReactMarkdown rehypePlugins={[rehypeRaw]} linkTarget={`_blank`}>
             {content}
           </ReactMarkdown>
         </div>
-        {sources && <div className="flex gap-2">
-          {sources?.split(`,`).map(source =>
-            <div key={source} className="flex self-start items-center text-xs border border-border border-solid gap-1 bg-card p-2 rounded-md">
-              <FileText height={14} width={14} />  {source}
-            </div>
-          )
-          }
-        </div>}
+        {sources && (
+          <div className="flex gap-2">
+            {sources?.split(`,`).map((source) => (
+              <div
+                key={source}
+                className="flex self-start items-center text-xs border border-border border-solid gap-1 bg-card p-2 rounded-md"
+              >
+                <FileText height={14} width={14} /> {source}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
-    </div>)
-}
+    </div>
+  );
+};
