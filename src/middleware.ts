49763cb6741ad689c73079bace4b1ccb65d25111
davisuga <daviciencia1@gmail.com>
2023-08-14 09:09:29 -0300

    added url middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,24 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+function urlMiddleware(request: Request) {
+  // Store current request url in a custom header, which you can read later
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set(`x-url`, request.url);
+
+  return NextResponse.next({
+    request: {
+      // Apply new request headers
+      headers: requestHeaders,
+    },
+  });
+}
 
 export default authMiddleware({
   signInUrl: `/sign-in`,
   publicRoutes: [`/`, `/sign-in`, `/sign-up`],
   ignoredRoutes: [`/privacy`],
+  beforeAuth: urlMiddleware,
 });
 
 export const config = {
