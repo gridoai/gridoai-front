a67cb82669d46c798607dade4f34b74e4119f6df
davisuga <daviciencia1@gmail.com>
2023-08-15 08:57:06 -0300

    Input resize fix

--- src/components/ui/resizingTextArea.tsx
+++ src/components/ui/resizingTextArea.tsx
@@ -1,20 +1,26 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
+
+export const maxResizableInputHeight = 180;
 const defaultStyle = {
   display: `block`,
   overflow: `hidden`,
   resize: `none`,
-  width: `100%`,
-} as const;
 
+  width: `100%`,
+  maxHeight: `${maxResizableInputHeight}px`,
+} as const;
 export const AutoHeightTextarea = ({
   style = defaultStyle,
+  onChangeHeight,
   ...props
 }: React.DetailedHTMLProps<
   React.TextareaHTMLAttributes<HTMLTextAreaElement>,
   HTMLTextAreaElement
->) => {
+> & {
+  onChangeHeight?: (height: number) => void;
+}) => {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const [currentValue, setCurrentValue] = useState(``); // you can manage data with it
 
@@ -22,8 +28,9 @@ export const AutoHeightTextarea = ({
     if (!textareaRef.current) return;
     textareaRef.current.style.height = `0px`;
     const scrollHeight = textareaRef.current.scrollHeight;
+    onChangeHeight?.(scrollHeight);
     textareaRef.current.style.height = scrollHeight + `px`;
-  }, [currentValue]);
+  }, [currentValue, onChangeHeight]);
 
   return (
     <textarea
