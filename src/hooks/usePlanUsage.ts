b34f12dcb4719f29c78da7c9f3a6a2d75295b55c
davisuga <daviciencia1@gmail.com>
2023-09-12 11:12:15 -0300

    using monthly based counting

--- src/hooks/usePlanUsage.ts
+++ src/hooks/usePlanUsage.ts
@@ -3,14 +3,14 @@ import {
   DOC_COUNTER_KEY,
   QUESTION_COUNTER_KEY,
   getDocumentCount,
-  getLastDayRequestCount,
+  getLastMonthRequestCount,
 } from "../services/rateLimit";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 
 export const usePlanUsage = () => {
   const [questionCount] = useLocalStorage(
     QUESTION_COUNTER_KEY,
-    getLastDayRequestCount()
+    getLastMonthRequestCount()
   );
   const [documentCount] = useLocalStorage(DOC_COUNTER_KEY, getDocumentCount());
   const { membershipList } = useOrganization();
