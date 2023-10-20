8943034bfdf38635ae3aec11b123b9a28c46ba07
davisuga <daviciencia1@gmail.com>
2023-10-20 17:02:27 -0300

    error fallback

---
+++ src/app/[locale]/(app)/documents/error.tsx
@@ -0,0 +1,8 @@
+import { UploadCard } from "./UploadCard";
+
+const Loading = () => (
+  <div className={`flex flex-col bg-card p-2 gap-4 md:p-4 rounded-xl`}>
+    <UploadCard />
+  </div>
+);
+export default Loading;
