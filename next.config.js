48d29d4d8d27b00f51cccebdf278acb88ce88704
davisuga <daviciencia1@gmail.com>
2023-12-12 11:57:09 -0300

    Remove remote rewrites in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const { withAxiom } = require(`next-axiom`);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
-  async rewrites() {
-    return [
-      {
-        source: `/remote/:path*`,
-        destination: process.env.REMOTE_SERVER + `/:path*`,
-      },
-    ];
-  },
 };
 
 module.exports = withAxiom(nextConfig);
