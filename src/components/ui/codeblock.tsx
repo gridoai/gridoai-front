4aedf488a0d8e6a2a4502116400fec3c0fe3d09d
davisuga <daviciencia1@gmail.com>
2023-12-17 16:21:58 -0300

    Refactor generateRandomString function

--- src/components/ui/codeblock.tsx
+++ src/components/ui/codeblock.tsx
@@ -49,10 +49,9 @@ export const programmingLanguages: languageMap = {
 
 export const generateRandomString = (length: number, lowercase = false) => {
   const chars = `ABCDEFGHJKLMNPQRSTUVWXY3456789`; // excluding similar looking characters like Z, 2, I, 1, O, 0
-  let result = ``;
-  for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
+  const result = Array.from({ length: chars.length }).reduce<string>((prev) => {
+    return prev + chars.charAt(Math.floor(Math.random() * chars.length));
+  }, ``);
   return lowercase ? result.toLowerCase() : result;
 };
 
