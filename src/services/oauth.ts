d7d8ea6b14dccb99a62cc370688559831b8dc722
davisuga <daviciencia1@gmail.com>
2023-12-19 09:41:57 -0300

    Refactor API and OAuth services
    This commit refactors the API and OAuth services to include a timeout of 5000 milliseconds when making requests to the server. This ensures that the requests do not hang indefinitely and improves the overall performance and responsiveness of the application.

--- src/services/oauth.ts
+++ src/services/oauth.ts
@@ -1,4 +1,12 @@
+import axios from "axios";
+
 export const isTokenValid = (token: string) =>
-  fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token}`
-  ).then((res) => res.ok);
+  axios
+    .get(
+      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token}`,
+      {
+        timeout: 5000,
+      }
+    )
+    .then((res) => res.status === 200)
+    .catch(() => false);
