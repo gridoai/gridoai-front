889219182dd3d69bfbd25c7682f4c71ac0e43655
davisuga <daviciencia1@gmail.com>
2023-12-23 15:18:25 -0300

    Update upload started message in en.ts and pt.ts

--- src/locales/pt.ts
+++ src/locales/pt.ts
@@ -106,7 +106,7 @@ export default {
     error: `Houve um erro ao carregar o arquivo`,
     success: `Arquivo carregado com sucesso`,
     justDrop: `Solte os arquivos aqui... `,
-    started: `Upload em andamento, aguarde`,
+    started: `Upload em andamento, você será notificado quando terminar`,
   },
   tryLater: `Por favor, tente novamente mais tarde`,
   upgradeNow: `Faça upgrade agora`,
