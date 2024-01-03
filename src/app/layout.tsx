f5b1a03ed24be19328683f11d0cfa51a7f2a2313
davisuga <daviciencia1@gmail.com>
2024-01-03 14:21:16 -0300

    Add SpeedInsights component to Root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,2 +1,9 @@
-const Root = ({ children }) => <>{children}</>;
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+const Root = async ({ children }: { children: React.ReactNode }) => (
+  <>
+    <SpeedInsights />
+    {children}
+  </>
+);
 export default Root;
