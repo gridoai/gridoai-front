335226bc8fd83bab3d46c5c55e3484dab53d5b1e
davisuga <daviciencia1@gmail.com>
2024-01-12 12:54:00 -0300

    Update documents max width

--- src/app/[locale]/(app)/documents/layout.tsx
+++ src/app/[locale]/(app)/documents/layout.tsx
@@ -1,5 +1,7 @@
 const Layout = ({ children }: { children: React.ReactNode }) => (
-  <div className="md:p-8 p-4 bg-background flex-1">{children}</div>
+  <div className="md:p-8 p-4 bg-background flex-1 max-w-7xl xl:w-[80rem] xl:mx-auto">
+    {children}
+  </div>
 );
 
 export default Layout;
