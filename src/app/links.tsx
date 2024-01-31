617e258ce1e252137402dc324a347828213c6a40
davisuga <daviciencia1@gmail.com>
2024-01-31 18:05:46 -0300

    Add whatsappBotLink to links.tsx

--- src/app/links.tsx
+++ src/app/links.tsx
@@ -1,2 +1,3 @@
 export const calendlyLink = `https://calendly.com/gridoai/30min`;
 export const whatsappLink = `https://wa.me/5585999065040`;
+export const whatsappBotLink = `https://wa.me/5531990779257`;
