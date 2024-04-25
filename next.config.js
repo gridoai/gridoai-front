dec607e7f1c6f95cc08a4e428f40965b1c477634
davisuga <daviciencia1@gmail.com>
2024-04-25 13:46:36 -0300

    back with rewrites

--- next.config.js
+++ next.config.js
@@ -7,6 +7,14 @@ const withBundleAnalyzer = require(`@next/bundle-analyzer`)({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
+  async rewrites() {
+    return [
+      {
+        source: `/remote/:path*`,
+        destination: process.env.REMOTE_SERVER + `/:path*`,
+      },
+    ];
+  },
 };
 
 const config = withBundleAnalyzer(withAxiom(nextConfig));
